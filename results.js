88cb165e6507fdc2110ecc20e8f546b78ed2e6a6
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-03-18 16:21:57 +0100

    adds chart

---
+++ results.js
@@ -0,0 +1,22 @@
+// l/obiettivo è quello di creare due arrayç uno per le risposte giuste
+//e uno per le risposte sbagliate. nei data di chartData
+//poi prenderò le length dei due arrai come dati
+
+const chartData = {
+  data: [62.5, 27.5],
+};
+
+const myChart = document.querySelector(".my-chart");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label: " answers",
+        data: chartData.data,
+      },
+    ],
+  },
+});
