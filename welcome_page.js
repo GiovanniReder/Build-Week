360744d61c0e9ae2ae89080ab3ba2966bf3dce9d
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-03-19 14:23:43 +0100

    fine

---
+++ welcome_page.js
@@ -0,0 +1,11 @@
+/*THE CHECKBOX NEEDS TO BE CHECKED FOR THE PROCEED BUTTON TO WORK*/ 
+const btn = document.getElementById("button")
+const check = document.getElementById("tmp-28");
+btn.addEventListener("click", () => {
+    if (check.checked==false){
+        alert("Please, check the box below!");
+      }
+    else{
+    btn.style.backgroundColor = "#9ffcfc";
+    window.location.href = "https://trello.com/b/eTcSUpLV/my-trello-board";}
+});
